5a0412f4f391b79e1feb231e80106a399e218b0a
Vincent-Viomesnil <vincent.viomesnil@axa.fr>
2023-08-19 11:57:35 +0200

    wip

--- src/main/java/recipes/RecipeApplication.java
+++ src/main/java/recipes/RecipeApplication.java
@@ -13,62 +13,63 @@ import java.util.Scanner;
 public class RecipeApplication {
     public static void main(String[] args) {
 
-        Epices epices = new Epices();
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Entrez les ingrédients (séparés par des virgules et un espace après chaque virgule) : ");
-        String ingredients = scanner.nextLine();
+                Epices epices = new Epices();
 
-        String url = "https://www.marmiton.org/recettes/index/categorie/plat-principal/";
-        for (int currentPage = 0; currentPage < 3; currentPage++) {
-            try {
-                Document document = Jsoup.connect(url + currentPage).timeout(6000).get();
-                Elements recipes = document.select(".recipe-card");
+                Scanner scanner = new Scanner(System.in);
+                System.out.print("Entrez les ingrédients (séparés par des virgules et un espace après chaque virgule) : ");
+                String ingredients = scanner.nextLine();
 
-                for (Element recipe : recipes) {
-                    String linkRecipe = recipe.select(".recipe-card-link").attr("href");
-                    Document doc2 = Jsoup.connect(linkRecipe).timeout(6000).get();
+                String url = "https://www.marmiton.org/recettes/index/categorie/plat-principal/";
+                for (int currentPage = 0; currentPage < 10; currentPage++) {
+                    try {
+                        Document document = Jsoup.connect(url + currentPage).timeout(6000).get();
+                        Elements recipes = document.select(".recipe-card");
 
-                    String recipeName = recipe.select(".recipe-card__title").text();
-                    Elements ingredientRecipe = doc2.select("meta[property=og:description]");
+                        for (Element recipe : recipes) {
+                            String linkRecipe = recipe.select(".recipe-card-link").attr("href");
+                            Document doc2 = Jsoup.connect(linkRecipe).timeout(6000).get();
 
-                    String ingredientsList = ingredientRecipe.attr("content");
-                    System.out.println("Ingrédients de la recette Avant : " + ingredientsList);
+                            String recipeName = recipe.select(".recipe-card__title").text();
+                            Elements ingredientRecipe = doc2.select("meta[property=og:description]");
 
-                    String[] ingredientsArray = ingredientsList.split(", ");
-                    StringBuilder recipeIngredients = new StringBuilder();
+                            String ingredientsList = ingredientRecipe.attr("content");
+                            System.out.println("Ingrédients de la recette Avant : " + ingredientsList);
 
-                    for (String ingredient : ingredientsArray) {
-                        ingredient = ingredient.toLowerCase().trim();
-                        if (!epices.getEpicesListes().contains(ingredient)) {
-                            recipeIngredients.append(ingredient).append(", ");
+                            String[] ingredientsArray = ingredientsList.split(", ");
+                            StringBuilder recipeIngredients = new StringBuilder();
+
+                            for (String ingredient : ingredientsArray) {
+                                ingredient = ingredient.toLowerCase().trim();
+                                if (!epices.getEpicesListes().contains(ingredient) && containsExactIngredient(ingredient, ingredients)) {
+                                    recipeIngredients.append(ingredient).append(", ");
+                                }
+                            }
+
+                            System.out.println("Ingrédients filtrés de la recette : " + recipeIngredients);
+
+                            if (recipeIngredients.length() > 0) {
+                                System.out.println("Nom de la recette : " + recipeName);
+                                System.out.println("Lien de la recette : " + linkRecipe);
+                            } else {
+                                System.out.println("Recette ignorée : " + recipeName);
+                            }
+
                         }
+                    } catch (IOException e) {
+                        e.printStackTrace();
                     }
-
-                    System.out.println("Ingrédients filtrés de la recette : " + recipeIngredients);
-
-                    if (containsExactIngredients(recipeIngredients.toString(), ingredients)) {
-                        System.out.println("Nom de la recette : " + recipeName);
-                        System.out.println("Ingrédients de la recette : " + recipeIngredients);
-                        System.out.println("Lien de la recette : " + linkRecipe);
-                    } else {
-                        System.out.println("Recette ignorée : " + recipeName);
-                    }
-
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
             }
-        }
-    }
 
-    private static boolean containsExactIngredients(String recipeIngredients, String selectedIngredients) {
-        String[] selectedIngredientsArray = selectedIngredients.split(", ");
-        for (String ingredient : selectedIngredientsArray) {
-            if (!recipeIngredients.toLowerCase().contains(ingredient.trim().toLowerCase())) {
-                return false; // Si un seul ingrédient n'est pas trouvé, la recette est ignorée
+            private static boolean containsExactIngredient(String ingredient, String selectedIngredients) {
+                String[] selectedIngredientsArray = selectedIngredients.split(", ");
+                for (String selectedIngredient : selectedIngredientsArray) {
+                    if (ingredient.equalsIgnoreCase(selectedIngredient.trim())) {
+                        return true; // Si l'ingrédient correspond exactement, la recette est incluse
+                    }
+                }
+                return false;
             }
         }
-        return true;
-    }
-}
+
